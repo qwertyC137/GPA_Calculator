5d72fa2f3cfe0bbaa52b719cf13eb8791effb177
qwerty <liuweike1998@gmail.com>
2018-02-06 01:16:29 +0800

    Initial commit

---
+++ GPA_Calculator.py
@@ -0,0 +1,27 @@
+import requests
+import re
+
+login = 'http://eamis.nankai.edu.cn/eams/login.action'
+grade = 'http://eamis.nankai.edu.cn/eams/myPlanCompl.action'
+username = input('用户名：')
+password = input('密  码：')
+
+data = {'username': username, 'password': password, 'session_locale': 'zh_CN'}
+
+s = requests.Session()  # 保存Cookies
+s.post(login, data = data)  # 模拟登陆
+
+t = s.get(grade).text  # 提取网页源代码
+
+targets = re.findall('10%.*?<td>(.*?)<.*?">.*?<.*?">(.*?)<.*?">(.*?)\s*?<', t, re.S)  # (name, credits, grade)
+
+target = 0.0
+
+for tuple in targets:
+    if tuple[2] not in ['--', '', '通过']:
+        sum += float(tuple[1])
+        target += float(tuple[1]) * float(tuple[2])
+
+target /= sum
+
+print ('学分绩（百分制）：', target)
---
+++ README.md
@@ -0,0 +1,12 @@
+# GPA_Calculator
+
+南开大学GPA计算器
+并没有什么卵用
+just for fun
+
+## 原理
+
+输入用户名密码
+用requests库模拟登陆
+直接在http://eamis.nankai.edu.cn/eams/myPlanCompl.action上爬取学分和成绩
+计算出学分绩（百分制）
